6ce1a91744b1787dcfc6697d278ca2179bb816b0
Lyle Foster <foster.lyle@gmail.com>
2017-07-06 13:52:58 +0400

    added part 11: 11th set of 10 exercises

---
+++ part_11_arrays/3_free_the_lizzard.rb
@@ -0,0 +1,9 @@
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
+#Remove 'lizard' from my_pets then print the value of my_pets.
+
+#Expected output:
+# I have a pet fish!
+
+my_pets.pop
+puts "I have a pet #{my_pets[0]}"
---
+++ part_11_arrays/5_what_color.rb
@@ -0,0 +1,12 @@
+colors = ['red', 'yellow', 'purple', 'green']
+#Use Array#each to iterate over colors and print each element.
+
+#Expected output:
+# I'm the color red!
+# I'm the color yellow!
+# I'm the color purple!
+# I'm the color green!
+
+colors.each do |color|
+  puts "I'm the color #{color}"
+end
